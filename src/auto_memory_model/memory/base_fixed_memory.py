598942774683c0cd7698734778bfabf8428ad032
jblack97 <76516724+jblack97@users.noreply.github.com>
2022-04-04 12:01:08 +0100

    Update base_fixed_memory.py

--- src/auto_memory_model/memory/base_fixed_memory.py
+++ src/auto_memory_model/memory/base_fixed_memory.py
@@ -20,8 +20,16 @@ class BaseFixedMemory(BaseMemory):
         last_mention_idx = torch.tensor([0 for i in range(self.num_cells)], requires_grad=False).to(self.device)
         return mem, ent_counter, last_mention_idx
 
+
     def interpret_coref_new_score(self, coref_new_scores):
         pred_max_idx = torch.argmax(coref_new_scores).item()
+        coref_new_scores1 = torch.cat([coref_new_scores[0:pred_max_idx], coref_new_scores[pred_max_idx+1:]])
+        pred_max2_idx = torch.argmax(coref_new_scores1).item()
+        coref_new_scores2 = torch.cat([coref_new_scores1[0:pred_max2_idx], coref_new_scores1[pred_max2_idx+1:]])
+        pred_max3_idx = torch.argmax(coref_new_scores2).item()
+        if len(coref_new_scores[coref_new_scores > 0]) > 3:
+        if (coref_new_scores2[pred_max3_idx] > (0.2* coref_new_scores[pred_max_idx])) & (coref_new_scores1[pred_max2_idx] > (0.8* coref_new_scores[pred_max_idx])):
+            return -1, 'i'
         if pred_max_idx < self.num_cells:
             # Coref
             return pred_max_idx, 'c'
